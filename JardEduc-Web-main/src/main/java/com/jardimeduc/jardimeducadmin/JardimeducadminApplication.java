85e14a4d2b93f010de7e36e8707a985767a7d0f0
Sarah Gomes <114115806+Saah1517@users.noreply.github.com>
2023-05-26 10:10:45 -0300

    TCC

---
+++ JardEduc-Web-main/src/main/java/com/jardimeduc/jardimeducadmin/JardimeducadminApplication.java
@@ -0,0 +1,13 @@
+package com.jardimeduc.jardimeducadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JardimeducadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JardimeducadminApplication.class, args);
+	}
+
+}
